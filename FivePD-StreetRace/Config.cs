3d9c3444c05e8d5ef82a3f2aa6b795cf57f75982
Zulfurix <Zulfurix@gmail.com>
2020-07-11 19:26:19 +0100

    Implementation of basic configurable attributes

---
+++ FivePD-StreetRace/Config.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CitizenFX.Core;
+using static CitizenFX.Core.Native.API;
+using Newtonsoft.Json;
+using System.IO;
+using Newtonsoft.Json.Linq;
+
+namespace FivePD_StreetRace
+{
+    class Config : BaseScript
+    {
+        public static int minAmountOfRacers;
+        public static int maxAmountOfRacers;
+
+        public static void LoadConfig()
+        {
+            string jsonText = LoadResourceFile("fivepd", "callouts/FivePD-StreetRaceConfig.json");
+            dynamic configFile = JsonConvert.DeserializeObject(jsonText);
+            try
+            {
+                minAmountOfRacers = (int)configFile["config"]["minAmountOfRacers"];
+                maxAmountOfRacers = (int)configFile["config"]["maxAmountOfRacers"];
+            }
+            catch (Exception ex)
+            {
+                minAmountOfRacers = 1;
+                maxAmountOfRacers = 3;
+            }
+
+        }
+    }
+}
